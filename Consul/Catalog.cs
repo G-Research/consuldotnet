da95baa263511413df7e37d7959fbf08de3ac076
Hovik Aghajanyan <haghajanyan@servicetitan.com>
2020-09-22 11:30:39 +0400

    Added functionality to search service by multiple tags

--- Consul/Catalog.cs
+++ Consul/Catalog.cs
@@ -19,6 +19,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -212,11 +213,25 @@ namespace Consul
         /// <returns>A list of service instances</returns>
         public Task<QueryResult<CatalogService[]>> Service(string service, string tag, QueryOptions q, CancellationToken ct)
         {
+            return Service(service, new[] {tag}, q, ct);
+        }
+        
+        /// <summary>
+        /// Service is used to query catalog entries for a given service
+        /// </summary>
+        /// <param name="service">The service ID</param>
+        /// <param name="tags">A tag to filter on</param>
+        /// <param name="q">Customized query options</param>
+        /// <param name="ct">Cancellation token for long poll request. If set, OperationCanceledException will be thrown if the request is cancelled before completing</param>
+        /// <returns>A list of service instances</returns>
+        public Task<QueryResult<CatalogService[]>> Service(string service, IList<string> tags, QueryOptions q, CancellationToken ct)
+        {
             var req = _client.Get<CatalogService[]>(string.Format("/v1/catalog/service/{0}", service), q);
-            if (!string.IsNullOrEmpty(tag))
+            if (tags?.Any() == true)
             {
-                req.Params["tag"] = new [] {tag};
+                req.Params["tag"] = tags;
             }
+
             return req.Execute(ct);
         }
 
