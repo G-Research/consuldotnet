7785184f305dc050308977c2e16db4a27f619579
Marcin Krystianc <marcin.krystianc@gmail.com>
2021-10-05 09:10:45 +0200

    harden tests initialization

--- Consul.Test/BaseFixture.cs
+++ Consul.Test/BaseFixture.cs
@@ -43,7 +43,7 @@ namespace Consul.Test
         {
             if (_ready) return;
 
-            var cancelToken = new CancellationTokenSource(TimeSpan.FromSeconds(5)).Token;
+            var cancelToken = new CancellationTokenSource(TimeSpan.FromSeconds(15)).Token;
 
             await Task.Run(async () =>
             {
@@ -52,8 +52,9 @@ namespace Consul.Test
                     cancelToken.ThrowIfCancellationRequested();
                     try
                     {
+                        var peers = await _client.Status.Peers();
                         var leader = await _client.Status.Leader();
-                        if (!string.IsNullOrEmpty(leader))
+                        if (peers.Length == 1 && peers[0] == leader)
                         {
                             _ready = true;
                         }
@@ -61,6 +62,8 @@ namespace Consul.Test
                     catch
                     {
                     }
+
+                    await Task.Delay(TimeSpan.FromSeconds(1), cancelToken);
                 }
             }, cancelToken);
         }
