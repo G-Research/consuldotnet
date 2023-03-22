9e60e404ddffa37be2e464e64641a46bb372d24a
Marcin Krystianc <marcin.krystianc@gmail.com>
2023-03-22 12:51:12 +0100

    bump

--- Consul.Test/AgentTest.cs
+++ Consul.Test/AgentTest.cs
@@ -707,6 +707,7 @@ namespace Consul.Test
             Assert.Equal(HealthStatus.Passing, checks.Response[check1Id].Status);
             Assert.Equal("test is ok", checks.Response[check1Id].Output);
             Assert.Equal(HealthStatus.Passing, checks.Response[check2Id].Status);
+
             Assert.Equal("All checks passing.", checks.Response[check2Id].Output);
         }
     }
