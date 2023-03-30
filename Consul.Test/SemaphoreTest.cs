0d63e53af99656c612995e67e3ac94a27896b8ea
Marcin Krystianc <marcin.krystianc@gmail.com>
2023-03-30 10:07:55 +0200

    contenderPool = 50

--- Consul.Test/SemaphoreTest.cs
+++ Consul.Test/SemaphoreTest.cs
@@ -234,7 +234,7 @@ namespace Consul.Test
         public async Task Semaphore_ContendFast(int repeat)
         {
             string keyName = "test/semaphore/contend_" + repeat.ToString();
-            const int contenderPool = 15;
+            const int contenderPool = 50;
 
             var acquired = new System.Collections.Concurrent.ConcurrentDictionary<int, bool>();
 
