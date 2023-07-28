80e51f1d808fcd8eabf2ce7bc8325db8c83d8a8b
winstxnhdw <winstxnhdw@gmail.com>
2023-07-28 17:10:53 +0800

    style(ICatalogEndpoint): apply lint suggestions

--- Consul/Interfaces/ICatalogEndpoint.cs
+++ Consul/Interfaces/ICatalogEndpoint.cs
@@ -29,20 +29,20 @@ namespace Consul
     /// </summary>
     public interface ICatalogEndpoint
     {
-        Task<QueryResult<string[]>> Datacenters(CancellationToken ct = default(CancellationToken));
-        Task<QueryResult<string[]>> Datacenters(QueryOptions q, CancellationToken ct = default(CancellationToken));
-        Task<WriteResult> Deregister(CatalogDeregistration reg, CancellationToken ct = default(CancellationToken));
-        Task<WriteResult> Deregister(CatalogDeregistration reg, WriteOptions q, CancellationToken ct = default(CancellationToken));
-        Task<QueryResult<CatalogNode>> Node(string node, CancellationToken ct = default(CancellationToken));
-        Task<QueryResult<CatalogNode>> Node(string node, QueryOptions q, CancellationToken ct = default(CancellationToken));
-        Task<QueryResult<Node[]>> Nodes(CancellationToken ct = default(CancellationToken));
-        Task<QueryResult<Node[]>> Nodes(QueryOptions q, CancellationToken ct = default(CancellationToken));
-        Task<WriteResult> Register(CatalogRegistration reg, CancellationToken ct = default(CancellationToken));
-        Task<WriteResult> Register(CatalogRegistration reg, WriteOptions q, CancellationToken ct = default(CancellationToken));
-        Task<QueryResult<CatalogService[]>> Service(string service, CancellationToken ct = default(CancellationToken));
-        Task<QueryResult<CatalogService[]>> Service(string service, string tag, CancellationToken ct = default(CancellationToken));
-        Task<QueryResult<CatalogService[]>> Service(string service, string tag, QueryOptions q, CancellationToken ct = default(CancellationToken));
-        Task<QueryResult<Dictionary<string, string[]>>> Services(CancellationToken ct = default(CancellationToken));
-        Task<QueryResult<Dictionary<string, string[]>>> Services(QueryOptions q, CancellationToken ct = default(CancellationToken));
+        Task<QueryResult<string[]>> Datacenters(CancellationToken ct = default);
+        Task<QueryResult<string[]>> Datacenters(QueryOptions q, CancellationToken ct = default);
+        Task<WriteResult> Deregister(CatalogDeregistration reg, CancellationToken ct = default);
+        Task<WriteResult> Deregister(CatalogDeregistration reg, WriteOptions q, CancellationToken ct = default);
+        Task<QueryResult<CatalogNode>> Node(string node, CancellationToken ct = default);
+        Task<QueryResult<CatalogNode>> Node(string node, QueryOptions q, CancellationToken ct = default);
+        Task<QueryResult<Node[]>> Nodes(CancellationToken ct = default);
+        Task<QueryResult<Node[]>> Nodes(QueryOptions q, CancellationToken ct = default);
+        Task<WriteResult> Register(CatalogRegistration reg, CancellationToken ct = default);
+        Task<WriteResult> Register(CatalogRegistration reg, WriteOptions q, CancellationToken ct = default);
+        Task<QueryResult<CatalogService[]>> Service(string service, CancellationToken ct = default);
+        Task<QueryResult<CatalogService[]>> Service(string service, string tag, CancellationToken ct = default);
+        Task<QueryResult<CatalogService[]>> Service(string service, string tag, QueryOptions q, CancellationToken ct = default);
+        Task<QueryResult<Dictionary<string, string[]>>> Services(CancellationToken ct = default);
+        Task<QueryResult<Dictionary<string, string[]>>> Services(QueryOptions q, CancellationToken ct = default);
     }
 }
