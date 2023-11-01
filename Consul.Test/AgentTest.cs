305256524596636393ca6fb46ad7c3e28ff98f56
octocat <octocat@users.noreply.github.com>
2023-11-01 09:23:22 +0000

    style: fix whitespaces

--- Consul.Test/AgentTest.cs
+++ Consul.Test/AgentTest.cs
@@ -997,7 +997,7 @@ namespace Consul.Test
         [Fact]
         public async Task Agent_Host()
         {
-            
+
             var agent = await _client.Agent.GetAgentHostInfo();
             Assert.True(agent.Response.Host.Os.Contains("windows") || agent.Response.Host.Os.Contains("linux") || agent.Response.Host.Os.Contains("mac"));
         }
