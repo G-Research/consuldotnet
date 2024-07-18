d80461a229eed356f7c5621bc470cbb4f8114c03
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-07-18 15:15:27 +0200

    set 1min timeout

--- Consul.Test/HealthTest.cs
+++ Consul.Test/HealthTest.cs
@@ -21,6 +21,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -139,11 +140,10 @@ namespace Consul.Test
                 ulong lastIndex = 0;
                 do
                 {
-                    var q = new QueryOptions { WaitIndex = lastIndex, };
-
+                    var q = new QueryOptions { WaitIndex = lastIndex};
                     // Use the Health.Connect method to query health information for Connect-enabled services
-                    checks = await _client.Health.Connect(destinationServiceID, "", false, q,
-                        null); // Passing null for the filter parameter
+                    checks = await _client.Health.Connect(destinationServiceID, "", false, q, null,
+                        new CancellationTokenSource(TimeSpan.FromMinutes(1)).Token);
                     Assert.Equal(HttpStatusCode.OK, checks.StatusCode);
                     lastIndex = checks.LastIndex;
                 } while (!checks.Response.Any());
