5e85ba4e7435c1b174e4875fdc4198f15a706be4
tamnjongLarry <larrytabeh@gmail.com>
2024-10-20 18:42:34 +0100

    fix wrong AdminPartition implementation in ConsulClient (#379)

--- Consul/Interfaces/IConsulClient.cs
+++ Consul/Interfaces/IConsulClient.cs
@@ -65,6 +65,6 @@ namespace Consul
         ICoordinateEndpoint Coordinate { get; }
         ISnapshotEndpoint Snapshot { get; }
         IDiscoveryChainEndpoint DiscoveryChain { get; }
-        IAdminPartitionEndpoint AdminPartitionEndpoint { get; }
+        IAdminPartitionEndpoint AdminPartition { get; }
     }
 }
